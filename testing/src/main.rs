9a8b4dd8116ad45cdd59452bc72c01e175f4311c
Markus Nokelainen <markus.nokelainen@tutors.es>
2024-12-13 21:11:07 +0200

    new guys testing

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,21 +1,53 @@
+struct Guy {
+    name: String,
+    age: u32,
+    country: String,
+    gender: String,
+    race: String,
+    allergies: String,
+}
+
 fn main() {
-    let mut todos: Vec<String> = vec![];
-    println!("Enter a comma-separated list of to-dos:\n");
+    let markus = Guy {
+        name: String::from("Markus"),
+        age: 69,
+        country: String::from("Finland"),
+        gender: String::from("Male"),
+        race: String::from("White"),
+        allergies: String::from("None"),
+    };
+    println!(" ");
 
+    println!("Name: {}", markus.name);
+    println!("Age: {}", markus.age);
+    println!("Country: {}", markus.country);
+    println!("Gender: {}", markus.gender);
+    println!("Race: {}", markus.race);
+    println!("Allergies: {}", markus.allergies);
 
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    for todo in input.trim().split(", ") {
-        todos.push(todo.to_string());
-    }
+    println!(" ");
 
-    // Print the to dos all on the same line seperated by commas
-    print!("To-dos: ");
-    for (i, todo) in todos.iter().enumerate() {
-        if i == todos.len() - 1 {
-            print!("{}", todo);
-        } else {
-            print!("{}, ", todo);
+    // Create function
+    fn new_guy(name: String, age: u32, country: String, gender: String, race: String, allergies: String) -> Guy {
+        Guy {
+            name,
+            age,
+            country,
+            gender,
+            race,
+            allergies,
         }
     }
+
+    let john = new_guy(String::from("John"), 42, String::from("USA"), String::from("Non-binary"), String::from("Asian"), String::from("Peanuts"));
+
+    println!("Name: {}", john.name);
+    println!("Age: {}", john.age);
+    println!("Country: {}", john.country);
+    println!("Gender: {}", john.gender);
+    println!("Race: {}", john.race);
+    println!("Allergies: {}", john.allergies);
+    
+    println!(" ");
+
 }
