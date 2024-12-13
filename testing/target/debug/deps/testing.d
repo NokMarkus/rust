babd398cc95e0e3b5a40f165d628beb9b2c9212f
Markus Nokelainen <markus.nokelainen@tutors.es>
2024-12-13 20:38:13 +0200

    rust

---
+++ testing/target/debug/deps/testing.d
@@ -0,0 +1,5 @@
+C:\Users\Ren\Documents\GitHub\rust\testing\target\debug\deps\testing.exe: src/main.rs
+
+C:\Users\Ren\Documents\GitHub\rust\testing\target\debug\deps\testing.d: src/main.rs
+
+src/main.rs:
---
+++ testing/target/debug/testing.d
@@ -0,0 +1,1 @@
+C:\Users\Ren\Documents\GitHub\rust\testing\target\debug\testing.exe: C:\Users\Ren\Documents\GitHub\rust\testing\src\main.rs
