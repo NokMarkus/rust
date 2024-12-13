8dbc5df70c0b72fe20bd9b39882a749da95f7e73
Markus Nokelainen <markus.nokelainen@tutors.es>
2024-12-13 20:55:51 +0200

    yay :D

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,5 +1,21 @@
-use std::io;
-
 fn main() {
-    let mut tasks = Vec::new();
+    let mut todos: Vec<String> = vec![];
+    println!("Enter a comma-separated list of to-dos:\n");
+
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    for todo in input.trim().split(", ") {
+        todos.push(todo.to_string());
+    }
+
+    // Print the to dos all on the same line seperated by commas
+    print!("To-dos: ");
+    for (i, todo) in todos.iter().enumerate() {
+        if i == todos.len() - 1 {
+            print!("{}", todo);
+        } else {
+            print!("{}, ", todo);
+        }
+    }
 }
---
+++ testing/target/debug/deps/testing-4b8068aa771ca385.d
@@ -0,0 +1,5 @@
+C:\Users\Ren\Documents\GitHub\rust\testing\target\debug\deps\libtesting-4b8068aa771ca385.rmeta: src/main.rs
+
+C:\Users\Ren\Documents\GitHub\rust\testing\target\debug\deps\testing-4b8068aa771ca385.d: src/main.rs
+
+src/main.rs:
